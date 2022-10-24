0c049883c5e2357f66d1726b27a23092ee885f61
Dani <danisteele1984@gmail.com>
2022-10-24 14:52:05 +1100

    intro to js

--- intro-to-js/script.js
+++ intro-to-js/script.js
@@ -1,6 +1,15 @@
 window.onload = function() {
     // start
-
+    let starterDiv = document.getElementById('starter-div');
+    let firstHeading = document.createElement('h2');
+    firstHeading.innerText = 'This is an H2 created with JS';
+    firstHeading.classList.add('js-header');
+    starterDiv.appendChild(firstHeading);
+    let secondHeading = document.createElement('h2');
+    secondHeading.innerText = 'This is another H2 created with JS';
+    secondHeading.classList.add('js-header');
+    secondHeading.id = 'second-h2';
+    starterDiv.appendChild(secondHeading);
     // end
 }
 
@@ -8,13 +17,14 @@ window.onload = function() {
 function showName(){
     console.log('showName');
     // start
-
+    let name = document.getElementById('name').value;
+    document.getElementById('greeting-text').innerText = 'Hi ' + name;
     // end
 }
 
 function changeColour(colour) {
     // start
-
+    document.body.style.backgroundColor = colour;
     // end
 }
 
@@ -24,6 +34,7 @@ function resetColour() {
 
 function toggleLightbulb() {
     // start
-
+    let lightbulb = document.getElementById('light');
+    lightbulb.classList.toggle("on");
     // end
 }
--- keyboard/script.js
+++ keyboard/script.js
@@ -1,23 +1,40 @@
 window.onload = function() {
     let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
     // start
-
+    let keyboard = document.getElementById('keyboard');
     // end
 
     alphabet.forEach(letter => {
         // start
-
+        let keyButton = document.createElement('input');
+        keyButton.type = 'button';
+        keyButton.value = letter;
+        keyButton.classList.add('key');
+        keyButton.id = letter;
+        keyButton.addEventListener('click', event => {
+            highlightLetter(event.target.id)
+        })
+        keyboard.appendChild(keyButton);
         // end
     });
 }
 
 // start
-
+document.addEventListener('keydown', event => {
+    highlightLetter(event.key)
+});
 // end
 
 
 function highlightLetter(letter){
     // start
-
+    console.log('letter');
+    let output = document.getElementById('output-text');
+    output.innerText = output.innerText + letter;
+    let key = document.getElementById(letter);
+    key.classList.toggle('selected');
+    setTimeout(function() {
+        key.classList.toggle('selected');
+    }, 150);
     // end
 }
